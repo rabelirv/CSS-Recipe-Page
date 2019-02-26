4dc2021dd6000f6cafddd2b65909980d81cebefc
github username <github email address>
2019-02-26 00:50:31 -0500

    more practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <head>
+<title>Quinoa and Kale Salad Recipe</title>
+</head>
+
+<body>
+
+<img src="https://s3.amazonaws.com/codecademy-content/courses/freelance-1/unit-2/salad.jpg" alt="Kale Caeasar Salad"/>
+<h1>Kale Caesar Quinoa Salad with Roasted Chicken</h1>
+<p className="description">Kale and quinoa provide a healthy base for roasted chicken topped with a light Caesar sauce.</p>
+
+<p id="cook-time">Total time: 45 minutes</p>
+
+<h2>Ingredients</h2>
+<ul className="ingredients">
+  <li>1/4 cup kale</li>
+  <li>1 cup Quinoa</li>
+  <li>2 tbsp Olive Oil</li>
+  <li>1 chicken breast</li>
+  <li>Caesar Dressing</li>
+</ul>
+
+<h2>Preparation</h2>
+<ol>
+  <li>
+    <p>Prepare quinoa and roast chicken until golden brown and 165 in middle.</p>
+    <p className="time">Time: 40 minutes</p>
+  </li>
+  <li>
+    <p>Toss quinoa, chicken, kale, and Caesar dressing until coated.</p>
+    <p className="time">Time: 4 minutes</p>
+  </li>
+  <li>
+    <p>Add walnuts and olive oil as garnish.</p>
+    <p className="time">Time: 1 minute</p>
+  </li>
+</ol>
+
+<p className="citation">Find this recipe and more <a href="http://www.myrecipes.com/recipe/kale-caesar-salad-chicken" target="_blank" className="external-link">here</a>.</p>
+
+</body>
       </div>
     );
   }
